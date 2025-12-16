f88fba3b40cb1c02ff3705f458e1713e6eff0ae4
huangxingqian <huangxingqian52@gmail.com>
2025-12-16 08:08:48 +0800

    update main.h

--- my_app/include/main.h
+++ my_app/include/main.h
@@ -2,5 +2,12 @@
 #define MAIN_H
 
 #include "utils.h"
+#define ERROR_EXIT(ret, msg) do { \
+    if (ret == -1) { \
+        fprintf(stderr, "%s failed: %s (errno=%d)\n", \
+                msg, strerror(errno), errno); \
+        exit(EXIT_FAILURE); \
+    } \
+} while (0)
 
 #endif // MAIN_H
